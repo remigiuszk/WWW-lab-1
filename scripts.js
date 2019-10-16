caea2b2cb20bf0e929b33990b099bb087ece2815
remigiuszk <remigiusz20kaminski@gmail.com>
2019-10-16 23:56:16 +0200

    zmiany

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var element=document.createElement('p');
+element.classList.add('tekst-center', 'tekst-uppercase');
+element.style.color = 'green';
+element.innerHTML= 'to jest przykladowy tekst';
+
+document.body.appendChild(element);
